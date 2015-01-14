294e219bffb4ecba0d5722418de5642ef851c00a
thomasharatyk <thomas.haratyk@gmail.com>
2015-01-14 23:44:52 +0000

    Misc: updated window title and removed interval.rb

--- app/assets/javascripts/game_engine/interval.rb
+++
@@ -1,9 +0,0 @@
-class Interval
-  def initialize(time=GameSettings.animation_ms, &block)
-    @interval = `setInterval(function(){#{block.call}}, time)`
-  end
- 
-  def stop
-    `clearInterval(#@interval)`
-  end
-end
